176a5e1658cc3cdf640892b7f3a90d8df8c240bd
Neven Kajić <nkajic@users.noreply.github.com>
2020-12-16 13:05:28 +0100

    Add files via upload

--- src/view/frontend/requirejs-config.js
+++ src/view/frontend/requirejs-config.js
@@ -16,6 +16,9 @@ var config = {
             },
             'Magento_Checkout/js/view/payment': {
                 'Rubic_CleanCheckoutTheme/js/mixin/payment-mixin': true
+            },
+            'Magento_InventoryInStorePickupFrontend/js/view/store-selector': {
+                'Rubic_CleanCheckoutTheme/js/mixin/store-selector-mixin': true
             }
         }
     }
